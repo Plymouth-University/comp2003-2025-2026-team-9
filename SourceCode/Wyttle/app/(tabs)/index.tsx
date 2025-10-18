43646b659c323d28cfb37542ce66394e78c4fd6b
MrabDflong <romangrixti03@gmail.com>
2025-10-18 12:12:33 +0100

    Skween

--- SourceCode/Wyttle/app/(tabs)/index.tsx
+++ SourceCode/Wyttle/app/(tabs)/index.tsx
@@ -22,7 +22,7 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try Me</ThemedText>
+        <ThemedText type="subtitle">Step 1: Try me</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
