4e519904ecb500c8fe182b70da125715940bb47a
MrabDflong <romangrixti03@gmail.com>
2026-03-06 17:50:59 +0000

    ENV changes for RevenueCat API keys

--- SourceCode/Wyttle/payment/RevenueCat.js
+++ SourceCode/Wyttle/payment/RevenueCat.js
@@ -1,16 +1,22 @@
 import { Platform } from 'react-native';
 import Purchases, { LOG_LEVEL } from 'react-native-purchases';
+import Constants from 'expo-constants';
 
 export default function initializeRevenueCat() {
   Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
 
   // Platform-specific API keys (prefixed with EXPO_PUBLIC_ in .env)
-  const iosApiKey = process.env.EXPO_PUBLIC_REVENUECAT_IOS_API_KEY;
-  const androidApiKey = process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY;
+  //const iosApiKey = process.env.EXPO_PUBLIC_REVENUECAT_IOS_API_KEY;
+  //const androidApiKey = process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY;
+
+  const iosApiKey = Constants.expoConfig?.extra?.revenueCatIosApiKey;
+  const androidApiKey = Constants.expoConfig?.extra?.revenueCatAndroidApiKey;
 
   if (Platform.OS === 'ios') {
     Purchases.configure({ apiKey: iosApiKey });
   } else if (Platform.OS === 'android') {
     Purchases.configure({ apiKey: androidApiKey });
+  } else {
+    console.error('RevenueCat API key is missing for the current platform.');
   }
 }
